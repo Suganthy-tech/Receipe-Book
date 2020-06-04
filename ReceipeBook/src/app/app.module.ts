477e4fd34f283ca67720b69855a50ab83619866d
Suganthy S <sugamaxy7777@gmail.com>
2020-06-04 19:28:51 +0530

    components creation

--- ReceipeBook/src/app/app.module.ts
+++ ReceipeBook/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReceipeComponent } from './components/receipe/receipe.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ReceipeDetailComponent } from './components/receipe/receipe-detail/receipe-detail.component';
+import { ReceipeItemComponent } from './components/receipe/receipe-item/receipe-item.component';
+import { ReceipeListComponent } from './components/receipe/receipe-list/receipe-list.component';
+import { ShoppingListEditComponent } from './components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceipeComponent,
+    ShoppingListComponent,
+    ReceipeDetailComponent,
+    ReceipeItemComponent,
+    ReceipeListComponent,
+    ShoppingListEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
