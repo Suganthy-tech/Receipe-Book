2e1bae6fc2fe39f442e4f7ab588d14c5e8dc3a95
Suganthy S <sugamaxy7777@gmail.com>
2020-06-04 22:38:40 +0530

    updated class bootstrap

--- ReceipeBook/src/app/components/receipe/receipe-list/receipe-list.component.ts
+++ ReceipeBook/src/app/components/receipe/receipe-list/receipe-list.component.ts
@@ -9,7 +9,7 @@ export class ReceipeListComponent implements OnInit {
   public receipeList:Receipe[]=[new Receipe(
     'A TestReceipe',
     'This is a simple test',
-    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbcgoodfood.com%2Frecipes%2Fcollection%2Fpasta&psig=AOvVaw0478ID2MHdHgkS4liyv0cj&ust=1591362315058000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNiFzYau6OkCFQAAAAAdAAAAABAD"
+    'https://pinchofyum.com/wp-content/uploads/Moroccan-Chickpea-Bowls-4.jpg'
   ) ];
   constructor() { }
 
